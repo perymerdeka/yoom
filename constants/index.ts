df0180b402ccc754a015b6276a77dd38ab3bd282
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2024-04-17 18:18:06 +0700

    Done: Sidebar

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks = [
+    {
+        imgUrl: "/icons/home.svg",
+        label: "Home",
+        route: "/"
+
+    },
+    {
+        imgUrl: "/icons/upcoming.svg",
+        label: "Upcoming",
+        route: "/upcoming"
+    },
+    {
+        imgUrl: "/icons/previous.svg",
+        label: "Previous",
+        route: "/previous"
+    },
+    {
+        imgUrl: "/icons/add-personal.svg",
+        label: "Personal Room",
+        route: "/personal-room"
+    },
+    {
+        imgUrl: "/icons/Video.svg",
+        label: "Recording",
+        route: "/recording",
+    },
+]
